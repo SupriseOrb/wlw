f5594be75ff392dae40697e65d8d3d6afdbb4378
SupriseOrb <molinakmey@gmail.com>
2019-08-30 16:57:18 -0700

    Creating Main Menu

---
+++ Drink Water Not Alcohol/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject controlPanel;
+    public GameObject creditsPanel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controlPanel.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape)){
+            CloseControlPanel();
+        }
+        if(creditsPanel.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape)){
+            CloseCreditsPanel();
+        }
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void OpenControlPanel(){
+        controlPanel.SetActive(true);
+    }
+
+    public void CloseControlPanel(){
+        controlPanel.SetActive(false);
+    }
+
+    public void OpenCreditsPanel(){
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCreditsPanel(){
+        creditsPanel.SetActive(false);
+    }
+
+    public void CreditButton(){
+
+    }
+}
